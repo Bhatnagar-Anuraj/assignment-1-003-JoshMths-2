c1daee14e055247ec522e04eab889a2e0f32b1e8
JoshMths <jm5424@drexel.edu>
2026-04-04 17:06:36 -0400

    Finished Assignment

--- assignment/scene_builder.py
+++ assignment/scene_builder.py
@@ -85,33 +85,83 @@ cmds.move(building_x, building_height / 2.0, building_z, building)
 #   - Name the object meaningfully with the 'name' parameter or cmds.rename().
 #   - Position it so it sits on the ground (not floating or buried).
 # ---------------------------------------------------------------------------
+# tower base dimensions
 tower_base_radius = 3
 tower_base_height = 7
 tower_base_x = 6
 tower_base_z = 3
 
+# create tower base 
 tower_base = cmds.polyCylinder(
 name = "Water Tower Base",
 radius = tower_base_radius,
 height = tower_base_height,
 )[0]
 
+# move tower base and position above plane
 cmds.move(tower_base_x, tower_base_height/2 , tower_base_z, tower_base)
 
 # ---------------------------------------------------------------------------
 # TODO: Add Object 3
 # ---------------------------------------------------------------------------
+# tower top dimensions
+W_tower_top_width = 5
+W_tower_top_height = 5
+W_tower_top_depth = 5
+W_tower_top_x = 6
+W_tower_top_z = 3
 
+# create tower top
+W_tower_top = cmds.polyCube(
+name = "Water Tower Top",
+width = W_tower_top_width,
+height = W_tower_top_height,
+depth = W_tower_top_depth,
+)[0]
+
+# move tower top and position above tower base
+cmds.move(W_tower_top_x, W_tower_top_height/2 + 7 , W_tower_top_z, W_tower_top)
+ 
 
 # ---------------------------------------------------------------------------
 # TODO: Add Object 4
 # ---------------------------------------------------------------------------
+# church base dimensions
+church_b_width = 4
+church_b_height = 6
+church_b_depth = 5
+church_b_x = -4
+church_b_z = -5
 
+# create church base
+church_b = cmds.polyCube(
+name = "Church Base",
+width = church_b_width,
+height = church_b_height,
+depth = church_b_depth,
+)[0]
+
+# move church base and position above plane
+cmds.move(church_b_x, church_b_height/2, church_b_z, church_b) 
 
 # ---------------------------------------------------------------------------
 # TODO: Add Object 5
 # ---------------------------------------------------------------------------
+# church top dimensions
+church_t_radius = 4
+church_t_height = 5
+church_t_x = -4
+church_t_z = -5
 
+# create church top
+church_t = cmds.polyCone(
+name = "Church Top",
+radius = church_t_radius,
+height = church_t_height,
+)[0]
+
+# move church top and position right above church base
+cmds.move(church_t_x, church_t_height/2 + 6, church_t_z, church_t)
 
 # ---------------------------------------------------------------------------
 # TODO (Optional): Add more objects to make your scene more interesting!
